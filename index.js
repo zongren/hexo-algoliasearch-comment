6353dfc963a63175ff3cded32294499249530f8a
zongren <zongren@live.com>
2017-04-10 16:13:48 +0800

    Fix a bug

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ var _ = require('lodash');
 var hexoUtil = require('hexo-util');
 var algoliasearch = require('algoliasearch');
 var async = require('async');
+var marked = require('marked');
+var assign = require('object-assign');
+
 var algoliaConfig = assign({
   enable: true
 }, hexo.config.algolia_comment);
@@ -10,8 +13,6 @@ var fields = algoliaConfig.fields;
 var enable = algoliaConfig.enable;
 var log = hexo.log;
 var comments = [];
-var marked = require('marked');
-var assign = require('object-assign');
 
 var markedOptions = assign({
   gfm: true,
@@ -33,8 +34,8 @@ marked.setOptions(markedOptions);
  */
 function processComment(comment) {
   var object = _.pick(comment, fields);
-  if(typeof comment.replyTarget != "undefined" && comment.replyTarget.length > 0){
-      comment.message = "@" + comment.replyTarget + "&nbsp;&nbsp;&nbsp;&nbsp;" + comment.message;
+  if (typeof comment.replyTarget != "undefined" && comment.replyTarget.length > 0) {
+    comment.message = "@" + comment.replyTarget + "&nbsp;&nbsp;&nbsp;&nbsp;" + comment.message;
   }
   object.message = marked(comment.message);
   object.objectID = comment._id;
@@ -53,7 +54,7 @@ function indexComments(index, comments) {
   var chunkedComments = _.chunk(comments, algoliaConfig.chunkSize || 5000);
 
   log.info('Indexing comments on Algolia...');
-  async.each(chunkedComments, index.saveObjects.bind(index), function(err) {
+  async.each(chunkedComments, index.saveObjects.bind(index), function (err) {
     if (err) {
       log.info('Error has occurred during indexing comments : ' + err);
       throw err;
@@ -61,22 +62,22 @@ function indexComments(index, comments) {
     log.info('Indexation done. ' + comments.length + ' comments indexed.');
   });
 }
-if(enable){
+if (enable) {
   processor.register('_data/*path', function static_processor(data) {
     var comment = JSON.parse(data.readSync());
     var object = processComment(comment);
-    if(typeof object.objectID != "undefined" && object.objectID.length > 0){
+    if (typeof object.objectID != "undefined" && object.objectID.length > 0) {
       comments.push(object);
     }
   });
 
-  hexo.on('generateBefore', function(post){
-    log.info("Start indexing comments.Comments length is "+comments.length);
+  hexo.on('generateBefore', function (post) {
+    log.info("Start indexing comments.Comments length is " + comments.length);
     // init index
     var client = algoliasearch(algoliaConfig.appId, algoliaConfig.adminApiKey);
     var index = client.initIndex(algoliaConfig.indexName);
-    
-    index.clearIndex(function(err) {
+
+    index.clearIndex(function (err) {
       if (err) {
         log.info('Error has occurred during clearing index : ' + err);
         return err;
